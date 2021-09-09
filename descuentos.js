71ce7f3ceec89a8a1f740b5e17d8b733227c4823
navinocrala <navinocrala@gmail.com>
2021-09-09 01:35:33 -0400

    Java script para calcular cupones de descuentos terminado

---
+++ descuentos.js
@@ -0,0 +1,42 @@
+
+
+function calcularPrecioConDescuento (precio, descuento){
+    const precioResultate = precio - (precio * descuento / 100);
+    return precioResultate;
+}
+
+function calcularCupones(cupon){
+     if (cupon == "JuanDC_es_Batman") {
+         descuento = 15;
+     }
+     else if (cupon == "pero_no_le_digas_a_nadie"){
+         descuento = 30;
+     }
+     else if (cupon == "es_un_secreto"){
+         descuento = 25;
+     }
+     else {
+         descuento = false;
+     }
+     return descuento;
+}
+
+
+function presentarPrecio(){
+    const inputPrecio = document.getElementById("inputPrecio");
+    const inputPrecioValue = inputPrecio.value;
+    const inputCupon = document.getElementById("inputCupon");
+    const inputCuponValue = inputCupon.value;
+
+    if (calcularCupones(inputCuponValue) == false){
+        resultado = document.getElementById("resultado");
+        resultado.innerText = "El cupón ingresado es inválido, favor verificar.";
+        return resultado.innerText;
+    }
+    else {
+        const precioDescontado = calcularPrecioConDescuento(inputPrecioValue, calcularCupones(inputCuponValue));
+        resultado = document.getElementById("resultado");
+        resultado.innerText = "El total a pagar es Gs. " + precioDescontado;
+        return resultado.innerText;
+    }
+}
